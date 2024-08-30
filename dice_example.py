8b2439b2dd1a4b35b4e3fa0fb75d2f34021fcdf1
Matt Benatan <mattbenatan@gmail.com>
2024-08-30 11:48:12 +0100

    Fixed bug in dice example. Fixed plotting.

--- dice_example.py
+++ dice_example.py
@@ -7,16 +7,14 @@ import seaborn as sns
 
 
 def sample_die(n_samples, sides=6):
-    return np.array([np.random.randint(6) + 1
+    return np.array([np.random.randint(sides) + 1
             for i in range(n_samples)])
 
 
 samples = sample_die(10) + 3
 sns.histplot(x=samples, stat="probability",
-             bins=6)
+             bins=6, discrete=True)
 plt.title("Dice example: 100 samples")
-plt.locator_params(nbins=6)
-plt.xticks([4, 5, 6, 7, 8, 9])
 
 
 
@@ -27,10 +25,9 @@ plt.close()
 
 
 samples = sample_die(1000) + 3
-sns.histplot(x=samples, stat="probability", bins=6)
+sns.histplot(x=samples, stat="probability", bins=6, discrete=True)
 plt.title("Dice example: 1000 samples")
-plt.locator_params(nbins=6)
-plt.xticks([4, 5, 6, 7, 8, 9])
+
 fig = plt.gcf()
 fig.set_size_inches(10, 6)
 plt.savefig("die_example_1000.png")
@@ -38,10 +35,9 @@ plt.close()
 
 
 samples = sample_die(100000) + 3
-sns.histplot(x=samples, stat="probability", bins=6)
+sns.histplot(x=samples, stat="probability", bins=6, discrete=True)
 plt.title("Dice example: 100,000 samples")
-plt.locator_params(nbins=6)
-plt.xticks([4, 5, 6, 7, 8, 9])
+
 fig = plt.gcf()
 fig.set_size_inches(10, 6)
 plt.savefig("die_example_100000.png")
